254e29da4bd303fc0f5c0dd5a63a76832fc402c7
yasminelias <yayielias12@gmail.com>
2025-08-18 18:47:32 -0300

    Implementación de EmailJS para envío real de correos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'calificaciones', loadChildren: () => import('./calificaciones/calificaciones.module').then(m => m.CalificacionesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
