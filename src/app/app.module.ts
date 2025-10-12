eaaca2d87cc3f0c7c92461e53249cb4c72e4f103
araceli <asoffulto@frba.utn.edu.ar>
2025-10-12 14:56:34 -0300

    pwa mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -37,6 +37,7 @@ import { TablaCargaNotasComponent } from './carga-finales/tabla-carga-notas/tabl
 import { QRCodeModule } from 'angularx-qrcode';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -86,6 +87,12 @@ import { environment } from '../environments/environment';
       cacheLocation: 'localstorage', // útil para mantener sesión después de reload
       useRefreshTokens: true
     }),
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
+      }),
   ],
   providers: [
     {
